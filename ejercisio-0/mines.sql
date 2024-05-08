047d8ae5448e53e3b9114e94b3ecfec371dd25b6
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-05-07 19:49:27 -0700

    Update mines.sql

--- ejercisio-0/mines.sql
+++ ejercisio-0/mines.sql
@@ -2,5 +2,5 @@ CREATE TABLE mines(
 mines_id NUMBER PRIMARY KEY ,
 mine_name VARCHAR2(50) NOT NULL,
 product_id NUMBER NOT NULL,
-FOREIGN KEY (product_id) REFERENCES product(product_id)
+FOREIGN KEY (product_id) REFERENCES products(products_id)
 );
